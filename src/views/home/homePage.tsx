154b4a72107f11da37f51bed8207d6f4b01476fb
ShocKWavEv1 <rodd.sal23@gmail.com>
2024-05-24 15:06:19 -0600

    full home page

--- src/views/home/homePage.tsx
+++ src/views/home/homePage.tsx
@@ -1,17 +1,33 @@
 "use client";
 
-import React from "react";
+import React, { useRef } from "react";
 import { Box } from "@chakra-ui/react";
 import { HomePageProps } from "./model";
 import Clients from "@/components/clients/clients";
+import NoiseGradient from "@/components/noiseGradient/noiseGradient";
+import Manifesto from "@/components/manifesto/manifesto";
+import Discover from "@/components/discover/discover";
+import Reel from "@/components/reel/reel";
+import Capture from "@/components/capture/capture";
+import ParallaxGrid from "@/components/parallaxGrid/parallaxGrid";
+import Team from "@/components/team/team";
 import ProjectsGrid from "@/components/projectsGrid/projectsGrid";
 import Footer from "@/components/footer/footer";
 
 const HomePage: React.FC<HomePageProps> = ({ contentData }) => {
+  const videoRef = useRef(null);
+
   return (
     <Box>
+      <NoiseGradient />
       <ProjectsGrid projectList={contentData.projects} />
       <Clients />
+      <Manifesto />
+      <Discover />
+      <Reel videoRef={videoRef} />
+      <Capture />
+      <ParallaxGrid parallaxGrid={contentData.parallaxGrid} />
+      <Team crew={contentData.crew} />
       <Footer />
     </Box>
   );
