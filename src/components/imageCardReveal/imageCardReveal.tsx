bb3ae70e7cd97a4189cf4f77cba84614f3db1706
ShocKWavEv1 <rodd.sal23@gmail.com>
2024-06-17 18:25:01 -0600

    image card reveal for mobile

--- src/components/imageCardReveal/imageCardReveal.tsx
+++ src/components/imageCardReveal/imageCardReveal.tsx
@@ -30,8 +30,7 @@ const ImageCardReveal: React.FC<ImageCardRevealProps> = ({
         <Box
           ref={ref}
           w="100%"
-          minH="500px"
-          h="600px"
+          h={["auto", "600px", "600px", "600px", "600px", "600px"]}
           display="flex"
           alignItems="center"
           justifyContent="center"
