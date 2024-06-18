d36da845e0060082c38a3784bc8075bf50ad0d10
ShocKWavEv1 <rodd.sal23@gmail.com>
2024-06-18 12:17:00 -0600

    loading for project detail page

---
+++ src/app/work/[projectId]/loading.tsx
@@ -0,0 +1,9 @@
+import { Box } from "@chakra-ui/react";
+
+export default function Loading() {
+  return (
+    <Box w="100%" h="100vh" bg="black" placeItems="center">
+      Hello Loading
+    </Box>
+  );
+}
