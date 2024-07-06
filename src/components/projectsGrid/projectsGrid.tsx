c9efe33ef214134ac7fc461841524268c9d937e4
ShocKWavEv1 <rodd.sal23@gmail.com>
2024-07-05 19:59:52 -0600

    project grid as client side

--- src/components/projectsGrid/projectsGrid.tsx
+++ src/components/projectsGrid/projectsGrid.tsx
@@ -1,18 +1,55 @@
+"use client";
+
 import { Box } from "@chakra-ui/react";
 import SectionHeader from "../sectionHeader/sectionHeader";
 import ColumnImages from "./columnImages/columnImages";
 import { useEffect, useState } from "react";
 import { ProjectsGridProps } from "./model";
+import { configSanity, handleImagesUrl } from "@/app/lib/sanity/sanity";
 
 const ProjectsGrid: React.FC<ProjectsGridProps> = ({ projectList }) => {
+  const [currentProjectList, setCurrentProjectList] = useState(projectList);
   const [projectFirstColumn, setProjectFirstColumn] = useState([]);
   const [projectSecondColumn, setProjectSecondColumn] = useState([]);
+
+  const { projectId, dataset } = configSanity;
+
+  const queries = {
+    projects: `*[ _type == "projects" ] | order(_createdAt asc)`,
+  };
+
+  const urls = {
+    projects: `https://${projectId}.api.sanity.io/v1/data/query/${dataset}?query=${encodeURIComponent(
+      queries.projects
+    )}`,
+  };
+
+  useEffect(() => {
+    const arr: any = [];
+    fetch(urls.projects)
+      .then((response) => response.json())
+      .then((data) => {
+        const items = data.result;
+
+        items.forEach(async (item: any) => {
+          const mainImage = handleImagesUrl(item.mainImage.asset._ref);
+          const base64 =
+            "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAIAAAAECAIAAAArjXluAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAJUlEQVR4nGO4c+d2bUsPAzMzQ3vPRAYnJ6f+vh6GBw8efP/+HQCseA2/ytznCwAAAABJRU5ErkJggg==";
+          item.mainImage = mainImage;
+          item.base64 = base64;
+          arr.push(item);
+          return item;
+        });
+
+        setCurrentProjectList(arr);
+      });
+  }, []);
 
   useEffect(() => {
     const firstColumn: any = [];
     const secondColumn: any = [];
 
-    projectList.forEach((element: any, index: any) => {
+    currentProjectList.forEach((element: any, index: any) => {
       if (index % 2 === 0) {
         firstColumn.push(element);
       } else {
@@ -23,7 +60,7 @@ const ProjectsGrid: React.FC<ProjectsGridProps> = ({ projectList }) => {
     setProjectFirstColumn(firstColumn);
     setProjectSecondColumn(secondColumn);
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [currentProjectList]);
 
   return (
     <Box
